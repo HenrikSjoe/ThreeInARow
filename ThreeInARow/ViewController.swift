ed7e904900a2c1d0caba9d421e2d99f8652c3dfc
HenrikSjoe <henrik.sjogren@iths.se>
2021-12-08 09:41:26 +0100

    fully functional

--- ThreeInARow/ViewController.swift
+++ ThreeInARow/ViewController.swift
@@ -39,9 +39,13 @@ class ViewController: UIViewController {
         
         imageList = [image1, image2, image3, image4, image5, image6, image7, image8, image9]
         
+        
     }
-
+    
     @IBAction func tap(_ sender: UITapGestureRecognizer) {
+        
+        // let pos = sender.view.tag
+        
         if !gameOver {
             if let imageView = sender.view as? UIImageView {
                 if imageView.image == nil{
@@ -49,19 +53,20 @@ class ViewController: UIViewController {
                         imageView.image = UIImage(named: "kryss")
                         winner()
                         changePlayer()
+                        // ai()
                     } else {
                         imageView.image = UIImage(named: "ring")
                         winner()
                         changePlayer()
-                        }
                     }
                 }
             }
         }
+    }
     
     func changePlayer (){
-            player1.toggle()
-        }
+        player1.toggle()
+    }
     
     func isDraw() -> Bool {
         for image in imageList{
@@ -72,14 +77,9 @@ class ViewController: UIViewController {
         return true
     }
     
+    
     func winner (){
-
-        if isDraw() == true {
-            gameOver.toggle()
-            draw.toggle()
-        }
-            
-         if image1.image == image2.image && image1.image == image3.image && image1.image != nil{
+        if image1.image == image2.image && image1.image == image3.image && image1.image != nil{
             gameOver.toggle()
         } else if  image4.image == image5.image && image4.image == image6.image && image4.image != nil{
             gameOver.toggle()
@@ -95,13 +95,15 @@ class ViewController: UIViewController {
             gameOver.toggle()
         } else if  image3.image == image6.image && image3.image == image9.image && image3.image != nil{
             gameOver.toggle()
-            
-      }
+        } else if isDraw() == true {
+            draw.toggle()
+        }
+        
         segueTowin()
     }
     
     func segueTowin(){
-        if gameOver{
+        if gameOver || draw {
             performSegue(withIdentifier: segueToWinVC, sender: self)
             resetBoard()
         }
@@ -114,22 +116,42 @@ class ViewController: UIViewController {
                 destinationVC.receivedWinner = "draw"
             }
             else if player1 && !draw {
-            destinationVC.receivedWinner = "X wins"
+                destinationVC.receivedWinner = "X wins"
             } else if !player1 && !draw  {
                 destinationVC.receivedWinner = "O wins"
             }
         }
     }
     
-      func resetBoard() {
-          Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { [self] (timer) in
-              for image in imageList{
-                  image.image = UIImage?(nil)
-                  gameOver.toggle()
-                  player1 = true
-                  draw = false
+    func resetBoard() {
+        imageList = [image1, image2, image3, image4, image5, image6, image7, image8, image9]
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: { [self]  in
+            for image in imageList{
+                image.image = UIImage?(nil)
+                gameOver = false
+                player1 = true
+                draw = false
             }
-        }
+        })
     }
+    
+    
+    //    func ai() {
+    //
+    //        if !player1 {
+    //            var randomInt = Int.random(in: 0...imageList.count-1)
+    //            var randomImage = imageList[randomInt]
+    //
+    //            if randomImage.image == nil {
+    //                randomImage.image = UIImage(named: "ring")
+    //            } else if randomImage.image != nil{
+    //                randomInt = Int.random(in: 0...imageList.count-1)
+    //                randomImage = imageList[randomInt]
+    //            }
+    //            player1.toggle()
+    //
+    //
+    //        }
+    //    }
+    
 }
-
